9a9caa9145ca426678fc1eabd5a18ebc3bcc302b
xudongjie <xudongjie@laixi.pro>
2022-06-10 17:32:52 +0800

    add alphas in painting

--- src/painting.ts
+++ src/painting.ts
@@ -3,6 +3,7 @@ import { PNG, PackerOptions } from "pngjs";
 
 interface Painting {
   colors: number[];
+  alphas: number[];
   height: number;
   width: number;
 }
@@ -12,12 +13,14 @@ export const fetchPainting = (path: string): Painting => {
   const height = png.height;
   const width = png.width;
   const colors = [];
+  const alphas = [];
   for (const i of Array(height * width).keys()) {
     const idx = i * 4;
     const rgb = (png.data[idx] << 16) + (png.data[idx + 1] << 8) + png.data[idx + 2]
     colors.push(rgb);
+    alphas.push(png.data[idx + 3]);
   }
-  return {colors, height, width};
+  return {colors, alphas, height, width};
 }
 
 // helpers

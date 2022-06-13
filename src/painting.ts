a87015c85aa01456139a382d2163cd4ca23a8133
xudongjie <xudongjie@laixi.pro>
2022-06-13 16:18:44 +0800

    revert adding color-id in fetchPainting

--- src/painting.ts
+++ src/painting.ts
@@ -1,19 +1,11 @@
 import fs from "fs";
 import { PNG, PackerOptions } from "pngjs";
 
-const RGBS = [
-  0x000000, 0xffffff, 0xd4d7d9, 0x898d90, 0x784102, 0xd26500, 0xff8a00,
-  0xffde2f, 0x159800, 0x8de763, 0x58eaf4, 0x059df2, 0x034cba, 0x9503c9,
-  0xd90041, 0xff9fab,
-];
 
 interface Painting {
   colors: number[];
-  raw: {
-    rgbs: number[];
-    alphas: number[];
-    grays: number[];
-  }
+  alphas: number[];
+  grays: number[];
   height: number;
   width: number;
 }
@@ -34,14 +26,11 @@ export const fetchPainting = (path: string): Painting => {
     const b = png.data[idx + 2];
     const a = png.data[idx + 3];
     const rgb = (r << 16) + (g << 8) + b
-
-    colors.push(RGBS.indexOf(rgb));
-
-    rgbs.push(rgb);
+    colors.push(rgb);
     alphas.push(a);
     grays.push(gray(r, g, b));
   }
-  return {colors, raw: {rgbs, alphas, grays}, height, width};
+  return {colors, alphas, grays, height, width};
 }
 
 export const steps = (painting: Painting): number[] => {

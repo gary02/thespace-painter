dd5014103ffebed00e990cd8d1a8447bc9f53a68
xudongjie <xudongjie@laixi.pro>
2022-06-13 14:27:18 +0800

    auto approve if needed

--- src/cli.ts
+++ src/cli.ts
@@ -2,42 +2,71 @@ import { ethers } from "ethers";
 
 import { fetchPainting } from "./painting";
 import { paint } from "./painter";
-import { abi } from "../abi/TheSpace.json";
+import { abi as thespaceABI } from "../abi/TheSpace.json";
+import { abi as registryABI } from "../abi/TheSpaceRegistry.json";
+import { abi as erc20ABI } from "../abi/ERC20.json";
 
 const main = async (path: string | undefined) => {
+  const thespaceAddr = process.env.THESPACE_ADDRESS;
+  const privateKey = process.env.PRIVATE_KEY;
+  const rpcUrl = process.env.PROVIDER_RPC_HTTP_URL;
   if (path === undefined) {
     console.error('please provide png file path');
     return;
   };
-  if (process.env.PROVIDER_RPC_HTTP_URL === undefined) {
-    console.error('please set PROVIDER_RPC_HTTP_URL env');
+  if (thespaceAddr === undefined) {
+    console.error('error: please set THESPACE_ADDRESS env');
     return;
   };
-  if (process.env.PRIVATE_KEY === undefined) {
-    console.error('please set PRIVATE_KEY env');
+  if (privateKey === undefined) {
+    console.error('error: please set PRIVATE_KEY env');
     return;
   };
-  if (process.env.THESPACE_ADDRESS === undefined) {
-    console.error('please set THESPACE_ADDRESS env');
+  if (rpcUrl === undefined) {
+    console.error('error: please set PROVIDER_RPC_HTTP_URL env');
     return;
   };
-  const thespace = initTheSpaceContract(
-    process.env.THESPACE_ADDRESS,
-    process.env.PROVIDER_RPC_HTTP_URL,
-    process.env.PRIVATE_KEY
+
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
+  const signer = new ethers.Wallet(privateKey, provider)
+
+  const thespace = new ethers.Contract(
+    thespaceAddr,
+    thespaceABI,
+    signer
   );
+  const registryAddr = await thespace.registry();
+
+  const registry = new ethers.Contract(
+    registryAddr,
+    registryABI,
+    signer
+  );
+
+  const currencyAddr = await registry.currency();
+  
+  const currency = new ethers.Contract(
+    currencyAddr,
+    erc20ABI,
+    signer
+  );
+
+  const balance = await currency.balanceOf(signer.address);
+  if ( balance.eq(0) ) {
+    console.error('error: this wallet address has no space tokens')
+    return;
+  }
+
+  const allowance = await currency.allowance(signer.address, registryAddr);
+
+  if ( allowance.eq(0) ) {
+    const tx = await currency.approve(registryAddr, balance);
+    await tx.wait();
+  }
 
   const painting = fetchPainting(path);
   //console.log(painting.colors);
-  await paint(fetchPainting(path), thespace);
-}
-
-// helpers
-
-const initTheSpaceContract = (address: string, rpcUrl: string, privateKey: string) => {
-  const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
-  const signer = new ethers.Wallet(privateKey, provider)
-  return new ethers.Contract(address, abi, signer)
+  await paint(painting, thespace);
 }
 
 main(process.argv[2]);

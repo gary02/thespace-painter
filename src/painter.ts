0aaa544e941a2e6a4c5e4972da61921d44241cff
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-06-14 17:55:26 +0800

    update painter

--- src/painter.ts
+++ src/painter.ts
@@ -8,12 +8,37 @@ const RGBS = [
   0xd90041, 0xff9fab,
 ];
 
-export const paint = async(painting: {colors: number[] ; height: number ;  width: number}, thespace: Contract) => {
-  const tokenId = 1;
-  const bidPrice = ethers.BigNumber.from('1000000000000000000');
-  const newPrice = ethers.BigNumber.from('1000000000000000000');
-  const color = 1;
+function rgb2cc(rgb: number) {
+  return RGBS.indexOf(rgb);
+}
 
-  const tx = await thespace.setPixel(tokenId, bidPrice, newPrice, color)
-  await tx.wait();
+export const paint = async(painting: {colors: number[] ; alphas: number[]; height: number ;  width: number}, thespace: Contract) => {
+    // , x: number, y: number, maxPrice: number  
+  const x = 400
+  const y = 480
+  const maxPrice = 20
+  const needPaint = (value: number) => value === 255
+  const pixelsOfRow = 1000;
+  let index = 0;
+  for (let rgb of painting.colors) {
+    if (needPaint(painting.alphas[index])) {
+      const pixelX = index % painting.width;
+      const pixelY = Math.floor(index / painting.width);
+      const tokenId = (y + pixelY - 1) * pixelsOfRow + (x + pixelX);
+      const bidPrice = ethers.BigNumber.from("1000000000000000000");
+      const newPrice = ethers.BigNumber.from("1000000000000000000");
+      const color = rgb2cc(rgb) + 1;
+      console.log({ index, x: x + pixelX, y: y + pixelY, color });
+      const price = await thespace.getPrice(tokenId);
+      const p = Number(ethers.utils.formatEther(price));
+      if (p <= maxPrice) {
+        const tx = await thespace.setPixel(tokenId, bidPrice, newPrice, color);
+        console.log({tx})
+        // await tx.wait();
+      }
+    }
+    
+    index += 1
+  }
+  
 }

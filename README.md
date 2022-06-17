ede0265e4d6dd06ba03d479d47fe22cea6e10c80
xudongjie <xudongjie@laixi.pro>
2022-06-17 11:20:26 +0800

    minor

--- README.md
+++ README.md
@@ -13,5 +13,5 @@ npm install
 PROVIDER_RPC_HTTP_URL=https://matic-mumbai.chainstacklabs.com \
 PRIVATE_KEY=******* \
 THESPACE_ADDRESS=0x68f02A0552e6B9010F34680746cd17E9F98fEC65 \
-npx ts-node src/cli.ts paint tests/data/test.png
+npx ts-node src/cli.ts paint tests/data/test.png --offset=100,100
 ```
--- src/painting.ts
+++ src/painting.ts
@@ -247,18 +247,11 @@ const grayscale = (rgb: RGB): number => (rgb[0]*299 + rgb[1]*587 + rgb[2]*114);
 
 const shuffle = (array: number[]): number[] => {
   let currentIndex = array.length,  randomIndex;
-
-  // While there remain elements to shuffle.
   while (currentIndex != 0) {
-
-    // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-
-    // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex], array[currentIndex]];
   }
-
   return array;
 }
